c1d981f82d265ead8e13c8c9194f479521e0dc27
RobertZK <technoguyrob@gmail.com>
2014-06-10 21:09:00 -0500

    actually run tests?

--- test/test_xmlmunger.rb
+++ test/test_xmlmunger.rb
@@ -1,5 +1,5 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 require 'test/unit'
-#load 'test_nested_hash.rb'
-#load 'test_parser.rb'
+load 'test_nested_hash.rb'
+load 'test_parser.rb'
